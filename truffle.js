2668aa0477c8dd008f848209c73e9fcfdbc18d04
J0YFUL <wanni4526@gmail.com>
2021-07-15 02:32:25 +0900

    first commit

---
+++ truffle.js
@@ -0,0 +1,16 @@
+const PrivateKeyConnector = require('connect-privkey-to-provider')
+const NETWORK_ID = '1001'
+const GASLIMIT = '20000000'
+const URL = `https://api.baobab.klaytn.net:8651`
+const PRIVATE_KEY = '0x018164d15488fc89d42636cc9d1edb696ba801e2a70733a43d54649e3cb0f11b' 
+
+module.exports = {
+  networks: {  
+    klaytn: {
+      provider: new PrivateKeyConnector(PRIVATE_KEY, URL),
+      network_id: NETWORK_ID,
+      gas: GASLIMIT,
+      gasPrice: null,
+    }
+  },
+}
